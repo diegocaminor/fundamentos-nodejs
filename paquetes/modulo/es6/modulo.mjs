f6803be2789eab6d11bacb0615f9d6df27d89f66
Diego Camino <diego.camino.r@gmail.com>
2020-03-08 22:55:40 -0500

    Construcción de módulos con require e import

---
+++ paquetes/modulo/es6/modulo.mjs
@@ -0,0 +1,8 @@
+function saludar() {
+    console.log("Hola Diego!!");
+}
+
+export default {
+    saludar,
+    prop1: "Soy un módulo experimental!"
+};
---
+++ paquetes/modulo/modulo.js
@@ -0,0 +1,8 @@
+ function saludar() {
+    console.log("Hola Diego");
+}
+
+module.exports = {
+    saludar,
+    prop1: "Hola que tal?"
+};
